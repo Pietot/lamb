1455ea149c9ff8b47b1995c7515a037cfb587805
Baptiste <baptiste.blasquez@gmail.com>
2025-05-25 17:48:22 +0200

    Updated return path

--- backend/pdo.php
+++ backend/pdo.php
@@ -1,10 +1,11 @@
 <?php
 $_ENV = parse_ini_file(__DIR__ . '/.env');
 
-function getPDO(): string|PDO
+function getPDO(): PDO
 {
     if (!isset($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'])) {
-        return json_encode(['success' => false, 'message' => 'Erreur serveur.']);
+        echo json_encode(['success' => false, 'message' => 'Erreur serveur.']);
+        exit;
     }
 
     try {
@@ -16,6 +17,7 @@ function getPDO(): string|PDO
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
-        return json_encode(['success' => false, 'message' => 'Erreur de connexion à la base de données.']);
+        echo json_encode(['success' => false, 'message' => 'Erreur de connexion à la base de données.']);
+        exit;
     }
 }
