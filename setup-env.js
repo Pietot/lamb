90814360b46746f9fd0937fd03cba50dca0a0e0c
Pietot <102545552+Pietot@users.noreply.github.com>
2025-06-03 13:02:47 +0200

    Added setup-env.js

---
+++ setup-env.js
@@ -0,0 +1,40 @@
+import { existsSync, readFileSync, appendFileSync } from "fs";
+import { join } from "path";
+
+function ensureEnvVars(filePath, requiredLines) {
+  let content = "";
+  if (existsSync(filePath)) {
+    content = readFileSync(filePath, "utf-8");
+    let addedLines = [];
+    const lines = content.split(/\r?\n/);
+    for (const line of requiredLines) {
+      const inSet = lines.some(existingLine => existingLine.includes(line));
+      if (!inSet) {
+        lines.push(line);
+        appendFileSync(filePath, line + "\n");
+        addedLines.push(line);
+      }
+    }
+    if (addedLines.length > 0) {
+      console.log(
+        `Missing variables found in ${filePath}.${"\n"} Added: ${"\n"}${addedLines.join("\n")}`
+      );
+    } else {
+      console.log(`All required variables are present in ${filePath}.`);
+    }
+  } else {
+    console.log(`${filePath} not found. Creating new file.`);
+  }
+}
+
+ensureEnvVars(join("backend", ".env"), [
+  "DB_HOST=",
+  "DB_NAME=",
+  "DB_USER=",
+  "DB_PASSWORD=",
+  "DOMAIN=http://localhost:5173",
+]);
+ensureEnvVars(".env.local", ["VITE_API_URL="]);
+ensureEnvVars(".env.prod", [
+  "VITE_API_URL=https://lamb.com/backend/",
+]);
