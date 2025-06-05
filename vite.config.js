e63ea7cbc3cc4ccb023924b99256831c3f209ced
Baptiste <baptiste.blasquez@gmail.com>
2025-06-05 09:33:34 +0200

    Added server proxy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  css: {
-    postcss: './postcss.config.js',
-  }
-})
+    css: {
+      postcss: "./postcss.config.js",
+    },
+    server: {
+      proxy: {
+        "/backend": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (p) => p,
+        },
+      },
+    },
+  };
+});
