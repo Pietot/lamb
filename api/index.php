72cd7e37ca87d08510fbe5d7299ca682ed9dc62f
Pietot <102545552+Pietot@users.noreply.github.com>
2025-06-05 20:55:38 +0200

    Removed useless echo json_encode

--- api/index.php
+++ api/index.php
@@ -9,32 +9,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 include_once __DIR__ . '/routes/utils/verify_auth_api.php';
 
-if (!verifyAuthAPI()) {
-    http_response_code(401);
-    echo json_encode(['success' => false, 'message' => 'Non autorisé']);
-    exit;
+if (verifyAuthAPI()) {
+    $_ENV = parse_ini_file(__DIR__ . '/routes/utils/.env');
+
+    $requestUri = $_SERVER['REQUEST_URI'];
+    $path = parse_url($requestUri, PHP_URL_PATH);
+
+    switch ($path) {
+        case $_ENV['API_ROUTE'] . 'verify_user':
+            require __DIR__ . '/routes/verify_user.php';
+            exit;
+
+        case $_ENV['API_ROUTE'] . 'login':
+            require __DIR__ . '/routes/login.php';
+            exit;
+
+        case $_ENV['API_ROUTE'] . 'logout':
+            require __DIR__ . '/routes/logout.php';
+            exit;
+
+        case $_ENV['API_ROUTE'] . 'get_table':
+            require __DIR__ . '/routes/get_table.php';
+            exit;
+    }
+
+    http_response_code(404);
+    echo json_encode(['success' => false, 'message' => 'Route introuvable.', 'route' => $path, 'referer' => $_ENV['API_ROUTE'] . 'login']);
 }
-
-$requestUri = $_SERVER['REQUEST_URI'];
-$path = parse_url($requestUri, PHP_URL_PATH);
-
-switch ($path) {
-    case '/lamb/api/verify_user':
-        require __DIR__ . '/routes/verify_user.php';
-        exit;
-
-    case '/lamb/api/login':
-        require __DIR__ . '/routes/login.php';
-        exit;
-
-    case '/lamb/api/logout':
-        require __DIR__ . '/routes/logout.php';
-        exit;
-
-    case '/lamb/api/get_table':
-        require __DIR__ . '/routes/get_table.php';
-        exit;
-}
-
-http_response_code(404);
-echo json_encode(['success' => false, 'message' => 'Route introuvable.', 'route' => $path]);
