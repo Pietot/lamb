f55e06f654225aee14fa3bfb8a368a590b45c593
Baptiste <baptiste.blasquez@gmail.com>
2025-06-05 08:28:38 +0200

    Fixed __dirname in postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,9 @@
 import { existsSync, writeFileSync } from "fs";
-import { join } from "path";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const backendEnvPath = join(__dirname, "backend", ".env");
 const backendEnvLines = [
