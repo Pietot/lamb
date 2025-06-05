81f16b6a8fc6b9da600a962bfc02e5bff3e24168
Pietot <102545552+Pietot@users.noreply.github.com>
2025-06-05 20:54:56 +0200

    Minor change

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
