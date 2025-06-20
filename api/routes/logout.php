df0d04c0af67fe2603afd5f7f3c00c014b6f0efd
Baptiste <baptiste.blasquez@gmail.com>
2025-06-20 16:04:49 +0200

    Moved getPDO for perf

--- api/routes/logout.php
+++ api/routes/logout.php
@@ -10,8 +10,6 @@ header('Access-Control-Allow-Headers: Content-Type');
 require_once __DIR__ . '/utils/pdo.php';
 require_once __DIR__ . '/utils/token.php';
 
-$pdo = getPDO();
-
 $id_utilisateur = $_POST['id_utilisateur'] ?? null;
 
 try {
@@ -19,6 +17,7 @@ try {
         echo json_encode(['success' => false, 'message' => 'Utilisateur non connecté.'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         exit;
     }
+    $pdo = getPDO();
     removeToken($pdo, $id_utilisateur);
     echo json_encode(['success' => true, 'message' => 'Déconnexion réussie.'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 } catch (Exception $e) {
