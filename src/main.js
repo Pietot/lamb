82e1ada6394a01cc2ec254b6c93ecc836d01eb01
Pietot <102545552+Pietot@users.noreply.github.com>
2025-07-15 20:01:09 +0200

    test mathis 2

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Toast from "vue-toastification";
 setTimeout(() => import("./style.css"), 0);
 setTimeout(() => import("vue-toastification/dist/index.css"), 0);
 
-console.log("arno ne finira jamais son dev");
+console.log("arno ne finira peut être son dev");
 
 const app = createApp(App);
 
